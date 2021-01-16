0fb24ad982f597034b81f7044327160260bb4c1d
canosaer <canosaer@gmail.com>
2021-01-16 13:41:23 -0500

    1.0

--- dist/js/tooltip.js
+++ dist/js/tooltip.js
@@ -1,10 +1,4 @@
-/* Parameters:
- * - selector: the DOM selector that will get turned into a hoverable tooltip trigger
- * - content: the text to be shown in the tooltip on hover
- */
-
-
- //IIFE vs Class
+//IIFE vs Class
 // const TooltipIIFE = (function(){
 
 //     var selector
@@ -39,23 +33,56 @@ class TooltipClass {
 
     setup() {    
         const tooltipElements = document.querySelectorAll(this.selector)
+        const bodyElement = document.querySelector(`body`)
+        bodyElement.style.position = `relative`
         tooltipElements.forEach(function(element) {
-            let tooltipWidth = (element.getBoundingClientRect().width)*1.5
-            element.style.position = `relative`
             let tooltipBox = document.createElement(`div`)
             tooltipBox.style.background = `black`
             tooltipBox.style.color = `white`
             tooltipBox.style.position = `absolute`
-            tooltipBox.style.width = `${tooltipWidth}px`
+            tooltipBox.style.width = `150px`
+            tooltipBox.style.height = `75px`
             tooltipBox.style.lineHeight = `1.4`
-            tooltipBox.style.top = `-50px`
-            tooltipBox.style.left = `-50px`
+            tooltipBox.style.display = `flex`
+            tooltipBox.style.alignItems = `center`
+            tooltipBox.style.justifyContent = `center`
+            tooltipBox.style.textAlign = `center`
+            tooltipBox.style.padding = `15px`
+            tooltipBox.style.opacity = `0`
+            tooltipBox.style.borderRadius = `10%`
+            tooltipBox.style.transition = `opacity 0.5s ease-out`
+            tooltipBox.style.top = `${window.scrollY+element.getBoundingClientRect().top+85}px`
+            tooltipBox.style.left = `${window.scrollX+element.getBoundingClientRect().left-40}px`
             tooltipBox.textContent = element.dataset[`tooltip`]
+            tooltipBox.classList.add(`slide-in`)
+            let arrowBox = document.createElement(`div`)
+            arrowBox.style.background = `black`
+            arrowBox.style.position = `absolute`
+            arrowBox.style.width = `15px`
+            arrowBox.style.height = `15px`
+            arrowBox.style.transform = `rotate(45deg)`
+            arrowBox.style.opacity = `0`
+            arrowBox.style.transition = `opacity 0.5s ease-out`
+            arrowBox.style.top = `${window.scrollY+element.getBoundingClientRect().top+152}px`
+            arrowBox.style.left = `${window.scrollX+element.getBoundingClientRect().left + (element.getBoundingClientRect().width/3)}px`
+            arrowBox.classList.add(`slide-in`)
             element.addEventListener(`mouseover`, function(){
-                element.appendChild(tooltipBox)
+                bodyElement.appendChild(tooltipBox)
+                bodyElement.appendChild(arrowBox)
+                // gsap.from(`.slide-in`, { duration: 1, ease: `expo`, xPercent: -100 });
+                setTimeout(function(){
+                    tooltipBox.style.opacity = `1`
+                    arrowBox.style.opacity = `1`
+                }, 1)
             })
             element.addEventListener(`mouseout`, function(){
-                tooltipBox.remove()
+                tooltipBox.style.opacity = `0`
+                arrowBox.style.opacity = `0`
+                setTimeout(function(){
+                    tooltipBox.remove()
+                    arrowBox.remove()
+                }, 500)
+                
             })
         })
     }
